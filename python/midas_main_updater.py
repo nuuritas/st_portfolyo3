299c5a951e2348c98f8b974c23fc0300349ab614
nuuritas <nuuritas@gmail.com>
2023-10-05 23:18:53 +0300

    yaml yeni

--- python/midas_main_updater.py
+++ python/midas_main_updater.py
@@ -1,5 +1,7 @@
 import subprocess
-
+print("starting foreks_fetch.py completed")
+subprocess.run(["python", "python/foreks_fetch.py"])
+print("foreks_fetch.py completed")
 subprocess.run(["python", "python/midas_raw_create.py"])
 print("midas_raw_create.py completed")
 subprocess.run(["python", "python/midas_create_summary.py"])
